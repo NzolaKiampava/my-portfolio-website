c071e14071131b9e00ef8d91a78bd6890acede15
Nzola Kiampava <nzolakiampava@gmail.com>
2022-10-25 18:21:24 -0300

    Update Services.jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -17,23 +17,23 @@ const Services = () => {
           <ul className="service__list">
             <li>
               <BsCheckLg className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit</p>
+              <p>Design technology consulting.</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit</p>
+              <p>User flow charts, diagrams.</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit</p>
+              <p>Brand and design system development.</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit</p>
+              <p>Content strategy.</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit</p>
+              <p>Information architecture</p>
             </li>
           </ul>
         </article>
@@ -47,27 +47,27 @@ const Services = () => {
           <ul className="service__list">
             <li>
               <BsCheckLg className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit</p>
+              <p>Content Management System</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit</p>
+              <p>Shopping Cart | Online Ordering System | Online Payments.</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit</p>
+              <p>Mobile-optimized Website | Email Newsletter</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit</p>
+              <p>Document Management System.</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit</p>
+              <p>Search Engine Optimization(SEO) | Web Hosting</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit</p>
+              <p>Password-Protected Pages | Client Area</p>
             </li>
           </ul>
         </article>
@@ -81,23 +81,23 @@ const Services = () => {
           <ul className="service__list">
             <li>
               <BsCheckLg className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit</p>
+              <p>Custom Android app development</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit</p>
+              <p>Maintenance and ongoing support.</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit</p>
+              <p>UI/UX design services.</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit</p>
+              <p>Mobile app porting.</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit</p>
+              <p>Power management, notification & geofencing services</p>
             </li>
           </ul>
         </article>
